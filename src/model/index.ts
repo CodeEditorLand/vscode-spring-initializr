8070b7aea0dc5420266a4add460a599103f94950
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-06 01:33:01 +0300

    squash!

--- src/model/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-export * from "./Interfaces";
-export * from "./pomxml/BomNode";
-export * from "./pomxml/DependencyNode";
-export * from "./pomxml/PomXml";
-export * from "./pomxml/RepositoryNode";
-export { serviceManager } from "./ServiceManager";
